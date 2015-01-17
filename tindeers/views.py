7a4692844cd8b6d08b7b64296c08cf7a8d242b04
Tyler Lubeck <tyler@tylerlubeck.com>
2015-01-17 07:11:56 -0500

    Get object or 404

--- tindeers/views.py
+++ tindeers/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 import json
 from tindeers.models import Product
 
@@ -11,24 +11,28 @@ from tindeers.models import Product
 def main_page(request):
     return render(request, 'tindeers/base.html', {})
 
+
 def create(request):
     return render(request, 'tindeers/create.html', {})
 
-def create_api(request):
-	video = request.POST.get('video',None)
-	description = request.POST.get('desc',None)
-	title = request.POST.get('title',None)
-	response_data = {}
-	if video and title and description:
-		p = Product()
-		p.video_link = video
-		p.description = description
-		p.title = title
-		p.creator_id = 1
-		p.save()
-		response_data["product"] = p.id
-	return HttpResponse(json.dumps(response_data), content_type="application/json")
 
-def display(request,vid):
-	p = Product.objects.get(pk=vid)
-	return render(request, 'tindeers/display.html', {'p':p})
+def create_api(request):
+    video = request.POST.get('video', None)
+    description = request.POST.get('desc', None)
+    title = request.POST.get('title', None)
+    response_data = {}
+    if video and title and description:
+        p = Product()
+        p.video_link = video
+        p.description = description
+        p.title = title
+        p.creator_id = 1
+        p.save()
+        response_data["product"] = p.id
+    return HttpResponse(json.dumps(response_data),
+                        content_type="application/json")
+
+
+def display(request, vid):
+    p = get_object_or_404(Product, pk=vid)
+    return render(request, 'tindeers/display.html', {'p': p})
